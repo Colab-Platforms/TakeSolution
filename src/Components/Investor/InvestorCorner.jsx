259c25e70629575a580bc4f4071162a47d966693
Amish8863 <amish2725@gmail.com>
2026-02-18 18:59:27 +0530

    replced /public/assets to /asset

--- src/Components/Investor/InvestorCorner.jsx
+++ src/Components/Investor/InvestorCorner.jsx
@@ -5,56 +5,56 @@ const InvestorCorner = () => {
   const eogmData = {
     title: 'Notice of the Extra Ordinary General Meeting',
     years: [
-      { year: '2021', link: '/public/assets/investordata/investor-corner/extra-ordinary-general-meeting/Final_EGM_Notice_Sept29_2021.pdf' },
-      { year: '2025', link: '/public/assets/investordata/investor-corner/extra-ordinary-general-meeting/Notice_EGM_TSL_2024.pdf' },
-      { year: '2026', link: '/public/assets/investordata/investor-corner/extra-ordinary-general-meeting/TS-EOGM-Notice-Final.pdf' }
+      { year: '2021', link: '/assets/investordata/investor-corner/extra-ordinary-general-meeting/Final_EGM_Notice_Sept29_2021.pdf' },
+      { year: '2025', link: '/assets/investordata/investor-corner/extra-ordinary-general-meeting/Notice_EGM_TSL_2024.pdf' },
+      { year: '2026', link: '/assets/investordata/investor-corner/extra-ordinary-general-meeting/TS-EOGM-Notice-Final.pdf' }
     ]
   };
 
   const egmVotingData = {
     title: 'EGM Voting Results',
     years: [
-      { year: '2021', link: '/public/assets/investordata/investor-corner/EGM Voting Results/Reg44_Voting_Results_261021.pdf' }
+      { year: '2021', link: '/assets/investordata/investor-corner/EGM Voting Results/Reg44_Voting_Results_261021.pdf' }
     ]
   };
 
   const agmNoticeData = {
     title: 'Notice of the Annual General Meeting',
     years: [
-      { year: '2025', link: '/public/assets/investordata/investor-corner/Notice of the Annual General Meeting/TAKE_Solutions_AGM_Notice_2025.pdf' },
-      { year: '2024', link: '/public/assets/investordata/investor-corner/Notice of the Annual General Meeting/TAKE_Solutions_AGM_Notice_2024.pdf' },
-      { year: '2023', link: '/public/assets/investordata/investor-corner/Notice of the Annual General Meeting/TAKE_Solutions_AGM_Notice_2023.pdf' },
-      { year: '2022', link: '/public/assets/investordata/investor-corner/Notice of the Annual General Meeting/TAKE_Solutions_AGM_Notice_2022.pdf' },
-      { year: '2021', link: '/public/assets/investordata/investor-corner/Notice of the Annual General Meeting/TAKE_Solutions_AGM_Notice_2021.pdf' },
-      { year: '2020', link: '/public/assets/investordata/investor-corner/Notice of the Annual General Meeting/TAKE_Solutions_AGM_Notice_2020.pdf' },
-      { year: '2019', link: '/public/assets/investordata/investor-corner/Notice of the Annual General Meeting/take-solution-notice-2019.pdf' },
-      { year: '2018', link: '/public/assets/investordata/investor-corner/Notice of the Annual General Meeting/Notice-2018-FINAL.pdf' },
-      { year: '2017', link: '/public/assets/investordata/investor-corner/Notice of the Annual General Meeting/notice-ts-ar-yr2017.pdf' },
-      { year: '2016', link: '/public/assets/investordata/investor-corner/Notice of the Annual General Meeting/notice-ts-ar-yr2016.pdf' },
-      { year: '2015', link: '/public/assets/investordata/investor-corner/Notice of the Annual General Meeting/Notice-TS-ar-yr2015.pdf' },
-      { year: '2014', link: '/public/assets/investordata/investor-corner/Notice of the Annual General Meeting/Notice-TS-ar-yr2014.pdf' },
-      { year: '2013', link: '/public/assets/investordata/investor-corner/Notice of the Annual General Meeting/Notice-TS-ar-yr2013.pdf' }
+      { year: '2025', link: '/assets/investordata/investor-corner/Notice of the Annual General Meeting/TAKE_Solutions_AGM_Notice_2025.pdf' },
+      { year: '2024', link: '/assets/investordata/investor-corner/Notice of the Annual General Meeting/TAKE_Solutions_AGM_Notice_2024.pdf' },
+      { year: '2023', link: '/assets/investordata/investor-corner/Notice of the Annual General Meeting/TAKE_Solutions_AGM_Notice_2023.pdf' },
+      { year: '2022', link: '/assets/investordata/investor-corner/Notice of the Annual General Meeting/TAKE_Solutions_AGM_Notice_2022.pdf' },
+      { year: '2021', link: '/assets/investordata/investor-corner/Notice of the Annual General Meeting/TAKE_Solutions_AGM_Notice_2021.pdf' },
+      { year: '2020', link: '/assets/investordata/investor-corner/Notice of the Annual General Meeting/TAKE_Solutions_AGM_Notice_2020.pdf' },
+      { year: '2019', link: '/assets/investordata/investor-corner/Notice of the Annual General Meeting/take-solution-notice-2019.pdf' },
+      { year: '2018', link: '/assets/investordata/investor-corner/Notice of the Annual General Meeting/Notice-2018-FINAL.pdf' },
+      { year: '2017', link: '/assets/investordata/investor-corner/Notice of the Annual General Meeting/notice-ts-ar-yr2017.pdf' },
+      { year: '2016', link: '/assets/investordata/investor-corner/Notice of the Annual General Meeting/notice-ts-ar-yr2016.pdf' },
+      { year: '2015', link: '/assets/investordata/investor-corner/Notice of the Annual General Meeting/Notice-TS-ar-yr2015.pdf' },
+      { year: '2014', link: '/assets/investordata/investor-corner/Notice of the Annual General Meeting/Notice-TS-ar-yr2014.pdf' },
+      { year: '2013', link: '/assets/investordata/investor-corner/Notice of the Annual General Meeting/Notice-TS-ar-yr2013.pdf' }
     ]
   };
 
   const agmVotingData = {
     title: 'AGM Voting Results',
     years: [
-      { year: '2022', link: '/public/assets/investordata/investor-corner/AGM Voting Results/Voting_Results_Scrutinizer_Report_AGM_261222.pdf' },
-      { year: '2021', link: '/public/assets/investordata/investor-corner/AGM Voting Results/Voting_Results_2021.pdf' },
-      { year: '2020', link: '/public/assets/investordata/investor-corner/AGM Voting Results/AGM PROCEEDINGS 2020.pdf' },
-      { year: '2019', link: '/public/assets/investordata/investor-corner/AGM Voting Results/agm-voting-08aug2019.pdf' },
-      { year: '2018', link: '/public/assets/investordata/investor-corner/AGM Voting Results/agm-voting-10aug2018.pdf' },
-      { year: '2017', link: '/public/assets/investordata/investor-corner/AGM Voting Results/agm-voting-11aug2017.pdf' }
+      { year: '2022', link: '/assets/investordata/investor-corner/AGM Voting Results/Voting_Results_Scrutinizer_Report_AGM_261222.pdf' },
+      { year: '2021', link: '/assets/investordata/investor-corner/AGM Voting Results/Voting_Results_2021.pdf' },
+      { year: '2020', link: '/assets/investordata/investor-corner/AGM Voting Results/AGM PROCEEDINGS 2020.pdf' },
+      { year: '2019', link: '/assets/investordata/investor-corner/AGM Voting Results/agm-voting-08aug2019.pdf' },
+      { year: '2018', link: '/assets/investordata/investor-corner/AGM Voting Results/agm-voting-10aug2018.pdf' },
+      { year: '2017', link: '/assets/investordata/investor-corner/AGM Voting Results/agm-voting-11aug2017.pdf' }
     ]
   };
 
   const postalBallotData = {
     title: 'Notice of the Postal Ballot',
     years: [
-      { year: '2023', link: '/public/assets/investordata/investor-corner/Notice of the Postal Ballot/Take_Postal_Ballot_Notice_FY22-23.pdf' },
-      { year: '2022', link: '/public/assets/investordata/investor-corner/Notice of the Postal Ballot/Postal_Ballot_Notice_TSL.pdf' },
-      { year: '2022', link: '/public/assets/investordata/investor-corner/Notice of the Postal Ballot/Take_Postal_Ballot_Notice_1.pdf' }
+      { year: '2023', link: '/assets/investordata/investor-corner/Notice of the Postal Ballot/Take_Postal_Ballot_Notice_FY22-23.pdf' },
+      { year: '2022', link: '/assets/investordata/investor-corner/Notice of the Postal Ballot/Postal_Ballot_Notice_TSL.pdf' },
+      { year: '2022', link: '/assets/investordata/investor-corner/Notice of the Postal Ballot/Take_Postal_Ballot_Notice_1.pdf' }
     ]
   };
 
