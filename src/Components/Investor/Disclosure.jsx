e791b3350e451486a76180b7d3b2da8cfed97ce7
Colab-Platforms <colabplatforms@gmail.com>
2026-04-14 15:21:46 +0530

    uploaded the investor data

--- src/Components/Investor/Disclosure.jsx
+++ src/Components/Investor/Disclosure.jsx
@@ -10,6 +10,7 @@ const Disclosure = () => {
 
     ///ujjwal uploaded FY- 26,25,20,19,18
     FY26: [
+      { type: 'Other Disclosures', date: '17-Feb-26', description: 'Reconciliation of Share Capital Audit report for the quarter ended March 31, 2026', link: 'https://cdn.shopify.com/s/files/1/0636/5226/6115/files/Reconciliation_of_Share_Capital_Audit_report_for_the_quarter_ended_March_31_2026.pdf?v=1776159817' },
       { type: 'Other Disclosures', date: '17-Feb-26', description: 'Reg 74(5) Q4', link: 'https://cdn.shopify.com/s/files/1/0636/5226/6115/files/Reg_74_5_Q4.pdf?v=1776144430' },
       { type: 'Other Disclosures', date: '17-Feb-26', description: 'PR Media_Release_Take_Solutions_Launches_Rs_50_Million_Innovation_Fund', link: '/assets/investordata/disclosure/disclosure-data/FY26/PR Media_Release_Take_Solutions_Launches_Rs_50_Million_Innovation_Fund.pdf' },
       { type: 'Other Disclosures', date: '17-Feb-26', description: 'Reg 30 Appointment of Auditor A. Raghavendra Rao & Associates', link: '/assets/investordata/disclosure/disclosure-data/FY26/Reg 30 Appointment of Auditor A. Raghavendra Rao & Associates.pdf' },
