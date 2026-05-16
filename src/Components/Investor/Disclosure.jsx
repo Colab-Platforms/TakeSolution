f6086da3c5dc76be359c14e7b4b9baacdaa7f348
Colab-Platforms <colabplatforms@gmail.com>
2026-05-16 15:18:22 +0530

    updated the data in the disclosure tab

--- src/Components/Investor/Disclosure.jsx
+++ src/Components/Investor/Disclosure.jsx
@@ -10,6 +10,7 @@ const Disclosure = () => {
 
 
     FY26: [
+      { type: 'Other Disclosures', date: '31-Mar-26', description: 'TAKE Prior Intimation of BM', link: 'https://cdn.shopify.com/s/files/1/0636/5226/6115/files/TAKE_Prior_Intimation_of_BM.pdf?v=1778924572' },
       { type: 'Other Disclosures', date: '31-Mar-26', description: 'Outcome of Board Meeting held on 13 May 2026', link: 'https://cdn.shopify.com/s/files/1/0636/5226/6115/files/Outcome_of_Board_Meeting_held_on_13_May_2026.pdf?v=1778670673' },
       { type: 'Other Disclosures', date: '31-Mar-26', description: 'Non-Applicability of Large Corporate Disclosure FY 26', link: 'https://cdn.shopify.com/s/files/1/0636/5226/6115/files/Non-Applicability_of_Large_Corporate_Disclosure_FY_26.pdf?v=1777531032' },
       { type: 'Other Disclosures', date: '17-Feb-26', description: 'Integrated Governance Q4', link: 'https://cdn.shopify.com/s/files/1/0636/5226/6115/files/Integrated_Governance_Q4.pdf?v=1777530077' },
